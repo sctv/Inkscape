5d6d9db78faa37249f134abd12c83bf74c49efa9
Brian Degger <brian.degger@gmail.com>
2018-03-16 17:23:24 +0000

    Update install.sh
    added commands for installing software to drive silhouette

--- files/install.sh
+++ files/install.sh
@@ -9,7 +9,13 @@
 add-apt-repository -y ppa:inkscape.dev/stable
 apt-get update -qq
 apt-get install -qy inkscape
-
+#####################################
+#	Commands for driving silhouette   #
+#							                  		#
+#####################################
+apt install python-usb
+wget https://github.com/fablabnbg/inkscape-silhouette/releases/download/v1.19/inkscape-silhouette_1.19-1_all.d
+sudo dpkg -i inkscape-silhouette_1.19-1_all.deb
 #####################################
 #	Add configs and needed stuff    	#
 #								                  	#
